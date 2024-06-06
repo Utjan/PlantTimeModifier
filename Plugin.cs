529dbcb398f45369d3e3521081c75596f296fc83
Utjan <utjan90@hotmail.com>
2024-06-06 23:56:09 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,171 @@
+﻿using Aki.Reflection.Patching;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using EFT;
+using EFT.InventoryLogic;
+using EFT.Quests;
+using EFT.UI;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PlantTimeModifier
+{
+    [BepInPlugin("com.utjan.PlantTimeModifier", "utjan.PlantTimeModifier", "1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+
+        internal static ConfigEntry<bool> enabledPlugin;
+        internal static ConfigEntry<float> timeMultiplierRepair;
+        internal static ConfigEntry<float> timeMultiplierHide;
+        internal static ConfigEntry<float> timeMultiplierProtect;
+
+        private void Awake() //Awake() will run once when your plugin loads
+        {
+            enabledPlugin = Config.Bind(
+                "1. Main Settings",
+                "Enable Mod",
+                true,
+                new ConfigDescription("Enable timer multipliers")
+            );
+
+            timeMultiplierRepair = Config.Bind(
+                "Main Settings",
+                "Repair objective Time Multiplier",
+                1f,
+                new ConfigDescription("Multiplies the duration when doing 'Repairing objective' task action. 0.5 = time is halved. 2.0 = time is doubled. 0 is instant", new AcceptableValueRange<float>(0, 5))
+            );
+
+            timeMultiplierHide = Config.Bind(
+                "Main Settings",
+                "Hide objective Time Multiplier",
+                1f,
+                new ConfigDescription("Multiplies the duration when doing 'Hiding objective' task action. 0.5 = time is halved. 2.0 = time is doubled. 0 is instant", new AcceptableValueRange<float>(0, 5))
+            );
+
+            timeMultiplierProtect = Config.Bind(
+                "Main Settings",
+                "Protect objective Time Multiplier",
+                1f,
+                new ConfigDescription("Multiplies the time it takes to protect task objective. Like when placing a MS2000 marker. 0.5 = time is halved. 2.0 = time is doubled. 0 is instant", new AcceptableValueRange<float>(0, 5))
+            );
+
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
+
+            new LeaveItemPatch().Enable();
+            new BeaconPlantPatch().Enable();
+        }
+    }
+
+    internal class LeaveItemPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GetActionsClass.Class1472), nameof(GetActionsClass.Class1472.method_0));
+        }
+
+        //Save list of objective zoneId's and plantTime to and make sure we're multiplying the base plantTime value on repeat actions
+        static List<KeyValuePair<string, float>> LeaveItemList = new List<KeyValuePair<string, float>>(); //zoneId, plantTime
+
+        [PatchPrefix]
+        static void Prefix(GetActionsClass.Class1472 __instance)
+        {
+            if (!Plugin.enabledPlugin.Value)
+                return;
+
+            float plantTime = -1;
+
+            foreach (var pair in LeaveItemList)
+            {
+                if (pair.Key == __instance.class1470_0.resultLeaveItem.zoneId)
+                {
+#if DEBUG
+                    Plugin.LogSource.LogWarning($"READING SAVED PLANTTIME {pair.Value} from zoneId {pair.Key}");
+#endif
+                    plantTime = pair.Value;
+                    break;
+                }
+            }
+            if (plantTime == -1)
+            {
+                LeaveItemList.Add(new KeyValuePair<string, float>(__instance.class1470_0.resultLeaveItem.zoneId, __instance.class1470_0.resultLeaveItem.plantTime));
+                plantTime = __instance.class1470_0.resultLeaveItem.plantTime;
+            }
+
+#if DEBUG
+            Plugin.LogSource.LogWarning($"BASE LEAVE ITEM TIME {__instance.class1470_0.resultLeaveItem.plantTime}");
+#endif
+
+            float multiplier;
+            if (__instance.isMultitool)
+            {
+#if DEBUG
+                Plugin.LogSource.LogWarning($"REPAIRING OBJECTIVE DETECTED");
+#endif
+                multiplier = Plugin.timeMultiplierRepair.Value;
+            }
+            else
+                multiplier = Plugin.timeMultiplierHide.Value;
+
+            __instance.class1470_0.resultLeaveItem.plantTime = plantTime * multiplier;
+
+#if DEBUG
+            Plugin.LogSource.LogWarning($"MODIFIED LEAVE ITEM TIME {__instance.class1470_0.resultLeaveItem.plantTime}");
+#endif
+        }
+    }
+
+    internal class BeaconPlantPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GetActionsClass.Class1473), nameof(GetActionsClass.Class1473.method_0));
+        }
+
+        //Save list of objective zoneId's and plantTime to and make sure we're multiplying the base plantTime value on repeat actions
+        static List<KeyValuePair<string, float>> ResultBeaconList = new List<KeyValuePair<string, float>>(); //zoneId, plantTime
+
+        [PatchPrefix]
+        static void Prefix(GetActionsClass.Class1473 __instance)
+        {
+            if (!Plugin.enabledPlugin.Value)
+                return;
+
+            float plantTime = -1;
+            foreach (var pair in ResultBeaconList)
+            {
+                if (pair.Key == __instance.resultBeacon.zoneId)
+                {
+#if DEBUG
+                    Plugin.LogSource.LogWarning($"READING SAVED PLANTTIME {pair.Value} from zoneId {pair.Key}");
+#endif
+                    plantTime = pair.Value;
+                    break;
+                }
+            }
+            if (plantTime == -1)
+            {
+                ResultBeaconList.Add(new KeyValuePair<string, float>(__instance.resultBeacon.zoneId, __instance.resultBeacon.plantTime));
+                plantTime = __instance.resultBeacon.plantTime;
+            }
+
+#if DEBUG
+            Plugin.LogSource.LogWarning($"BASE BEACON PLANT TIME {__instance.resultBeacon.plantTime}");
+#endif
+
+            __instance.resultBeacon.plantTime = plantTime * Plugin.timeMultiplierProtect.Value;
+
+#if DEBUG
+            Plugin.LogSource.LogWarning($"MODIFIED BEACON PLANT TIME {__instance.resultBeacon.plantTime}");
+#endif
+        }
+    }
+}
